37c5b42c4fdec6a3c52adb4e9c0412e7cbe660bf
Kamaro Lambert <kamaroly@gmail.com>
2015-05-06 12:12:03 +0200

    Separated phone js with html

---
+++ js/phone-form.js
@@ -0,0 +1,69 @@
+ $(function() {
+      /** Only allow numbers in the phone-num field */
+       $(".phone-num").keydown(function (e) {
+        // Allow: backspace, delete, tab, escape, enter and .
+        if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
+             // Allow: Ctrl+A, Command+A
+            (e.keyCode == 65 && ( e.ctrlKey === true || e.metaKey === true ) ) || 
+             // Allow: home, end, left, right, down, up
+            (e.keyCode >= 35 && e.keyCode <= 40)) {
+                 // let it happen, don't do anything
+                 return;
+        }
+        // Ensure that it is a number and stop the keypress
+        if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+            e.preventDefault();
+        }
+      });
+
+      var validatePhone = function() {
+          // Get the input phone number
+         var phoneNumber = $('.phone-num').val();
+          
+         // Get the first 3 phone numeric
+         var phoneType   = phoneNumber.substr(0,3);
+
+          // First check if the user provided country code
+          if((phoneType == '+25' || phoneType=='250') && phoneNumber.length>4)
+          {
+            phoneType    = phoneNumber.substr(3,3);
+          }
+          // check the length of the phone 
+          if (phoneType.length<3) 
+            {
+
+            $('.mobile').removeClass('tigo');
+            $('.mobile').removeClass('mtn');
+            $('.mobile').removeClass('airtel');
+              return;
+            };
+
+        switch(phoneType){
+          case  '072':
+            // if the expiry is valid add the identified class
+            $('.mobile').addClass('tigo');
+            break;
+          case  '078':
+            $('.mobile').addClass('mtn');
+            break;
+          case  '073':
+            $('.mobile').addClass('airtel');
+            break;
+          default:
+            break;
+        }
+      };
+ 
+   $('.phone-num').bind('change paste keyup', function() 
+   {
+      validatePhone();
+   });
+    
+      $('<input>', {
+                'type' : 'tel',
+                'name' : 'msisdn',
+                'id'   : 'phone-num', 
+                'value': '250', 
+            // appending the created element t '#content_container':
+            }).appendTo('#content_container');
+});
